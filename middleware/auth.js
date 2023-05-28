0d946fe108d6d5553fe36d2d5380b5109f04806a
Huynhle <huynh.le_1833446@hcmut.edu.vn>
2023-05-28 15:49:08 +0700

    chuyen sang tieng viet middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
   const token = req.header('Authorization');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (!token) return res.status(401).send('Từ chối truy cập vì không cung cấp token.');
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (ex) {
-    res.status(400).send('Invalid token.');
+    res.status(400).send('Token không hợp lệ.');
   }
 }
 
