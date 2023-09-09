526cda5fc0ef3672d99231837c1509c3926fafac
lehuynh <Huynhle1833446@gmail.com>
2023-09-09 10:06:26 +0700

    loai api login ko can middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).send('Từ chối truy cập vì không cung cấp token.');
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+  console.log("url", req.originalUrl)
+  if ( req.originalUrl != "/api/auth/login") {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).send('Từ chối truy cập vì không cung cấp token.');
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (ex) {
+      res.status(400).send('Token không hợp lệ.');
+    }
+  } else {
     next();
-  } catch (ex) {
-    res.status(400).send('Token không hợp lệ.');
   }
 }
 
