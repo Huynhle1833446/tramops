a0cab0f6959685c2d96440c459428862122afc2d
Lê Vinh <nguyenhuulevinh@LeDinh-MacPro.local>
2023-09-10 14:13:16 +0700

    table car

--- apis/user.js
+++ apis/user.js
@@ -1,24 +1,24 @@
-const bcrypt = require ('bcryptjs');
-const {hashedPassword, hashPassword} = require ("../utils/funtions.js")
-const TramDBConnection = require ("../utils/connection");
+const bcrypt = require('bcryptjs');
+const { hashedPassword, hashPassword } = require("../utils/funtions.js")
+const TramDBConnection = require("../utils/connection");
 const moment = require("moment");
 
 
 module.exports = class APIUser {
-    constructor() {
-      this.tramDB = new TramDBConnection();
-    }
+  constructor() {
+    this.tramDB = new TramDBConnection();
+  }
 
-    getAllUser = async (req) => {
-        return new Promise(async (resolve, reject) => {
-          const {current, pageSize} = req.body;
-            let valueOffset = (current - 1) * pageSize;
-            try {
-              const getTotal = await this.tramDB.runQuery('SELECT COUNT(id) as total FROM users');
-              const total = getTotal.rows[0].total;
-              valueOffset = valueOffset > total ? total : valueOffset;
+  getAllUser = async (req) => {
+    return new Promise(async (resolve, reject) => {
+      const { current, pageSize } = req.body;
+      let valueOffset = (current - 1) * pageSize;
+      try {
+        const getTotal = await this.tramDB.runQuery('SELECT COUNT(id) as total FROM users');
+        const total = getTotal.rows[0].total;
+        valueOffset = valueOffset > total ? total : valueOffset;
 
-              const list = await this.tramDB.runQuery(`SELECT last_name || ' ' || first_name AS name,
+        const list = await this.tramDB.runQuery(`SELECT last_name || ' ' || first_name AS name,
                           u.address,
                           u.id AS key,
                           DATE_PART('year', AGE(current_date, u.date_of_birth)) AS age,
@@ -30,63 +30,69 @@ module.exports = class APIUser {
                           u.email,
                           u.gender,
                           c.number_plate, 
+                          c.name as name_car,
                           u.image
                   FROM users u
                   LEFT JOIN cars c ON c.id = u.car_id
                   WHERE u.role != 'admin'
-                  ORDER BY u.register_at DESC, u.id ASC
+                  ORDER BY u.register_at DESC, u.id DESC
                   OFFSET $1 ROWS 
                   LIMIT $2; `, [valueOffset, pageSize]);
-              if(list.rowCount > 0) {
-                resolve({
-                  data: list.rows,
-                  pagination: {current, pageSize, total},
-                })
-              } else {
-                reject('Không có dữ liệu!')
-              }
-            } catch (e) {
-                reject(e);
-            }
-        });
-    };
-    createUser = async (req) => {
-        return new Promise(async (resolve, reject) => {
+        if (list.rowCount > 0) {
+          resolve({
+            data: list.rows,
+            pagination: { current, pageSize, total },
+          })
+        } else {
+          reject('Không có dữ liệu!')
+        }
+      } catch (e) {
+        reject(e);
+      }
+    });
+  };
+  createUser = async (req) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let { lastName, firstName, username, password, phone, birthday, address, gender } = req.body;
+
+        const queryCheck = `SELECT * FROM users WHERE username = $1`;
+        const check = await this.tramDB.runQuery(queryCheck, [username]);
+        if (check.rowCount > 0) reject('Tên đăng nhập đã tồn tại!')
+        else {
+          password = password || process.env.PASSWORD_DEFAULT;
+
+          const hashedPassword = await hashPassword(password);
+
           try {
-              let { lastName, firstName, username, password, phone, birthday, address, gender } = req.body;
+            await this.tramDB.runQuery('INSERT INTO users (last_name, first_name, username, password, phone, register_at, date_of_birth, address, gender) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id', [lastName, firstName, username, hashedPassword, phone, 'NOW()', birthday, address, gender]);
 
-              const queryCheck = `SELECT * FROM users WHERE username = $1`;
-              const check = await this.tramDB.runQuery(queryCheck, [username]);
-              if(check.rowCount > 0) reject('Tên đăng nhập đã tồn tại!')
 
-              password = password || process.env.PASSWORD_DEFAULT;
-              
-              const hashedPassword = await hashPassword(password);
-
-              await this.tramDB.runQuery('INSERT INTO users (last_name, first_name, username, password, phone, register_at, date_of_birth, address, gender) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id', [lastName, firstName, username, hashedPassword, phone, 'NOW()', birthday, address, gender]);
-
-             
-              resolve({
-                msg: `Chúc mừng bạn đã tạo thông tin người dùng ${username} thành công`
-              })
-            } catch (e) {
-                reject(e);
-            }
-        });
-    };
-    lockUnlock = async (req) => {
-      return new Promise(async (resolve, reject) => {
-        const {action, id} = req.body;
-        const value = action === 'lock' ? 1 : 0;
-          try {
-            const queryUpdate = await this.tramDB.runQuery(`UPDATE users SET is_locked = $1 WHERE id = $2 RETURNING users.fullname as name`, [value, id]);
-
-            console.log("🚀 ~ file: user.js:74 ~ APIUser ~ returnnewPromise ~ queryUpdate.rows:", queryUpdate.rows[0].name)
-            resolve(queryUpdate.rows[0].name)
-            
-          } catch (e) {
-              reject(e);
+            resolve({
+              msg: `Chúc mừng bạn đã tạo thông tin người dùng ${username} thành công`
+            })
+          } catch (error) {
+            reject(error)
           }
-      });
+        }
+      } catch (e) {
+        reject(e);
+      }
+    });
+  };
+  lockUnlock = async (req) => {
+    return new Promise(async (resolve, reject) => {
+      const { action, id } = req.body;
+      const value = action === 'lock' ? 1 : 0;
+      try {
+        const queryUpdate = await this.tramDB.runQuery(`UPDATE users SET is_locked = $1 WHERE id = $2 RETURNING users.fullname as name`, [value, id]);
+
+        console.log("🚀 ~ file: user.js:74 ~ APIUser ~ returnnewPromise ~ queryUpdate.rows:", queryUpdate.rows[0].name)
+        resolve(queryUpdate.rows[0].name)
+
+      } catch (e) {
+        reject(e);
+      }
+    });
   };
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require ("express");
 const authRoutes = require ("./auth.js");
 const userRoutes = require ("./user.js");
+const carRoutes = require ("./car.js");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/car", carRoutes);
 
 module.exports = router;
